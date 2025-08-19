c184eb01eca011419247111c669eaffaa79d69a2
rajeshboyina <rboyina.123@gmail.com>
2025-08-19 22:38:04 +0530

    completed Missing range .cpp file

--- c++/Hashing/MissingRange.cpp
+++ c++/Hashing/MissingRange.cpp
@@ -2,10 +2,22 @@
 #include<unordered_set>
 #include<climits>
 using namespace std;
-int Missing(int *arr)
+/*
+Given an array of size N. Let min and max be the minimum and maximum in the array respectively. Task is to find how many number should be added to the given array such that all the element in the range [min, max] occur at-least once in the array.
+
+Examples:
+
+Input : arr[] = {4, 5, 3, 8, 6}
+Output : 1
+Only 7 to be added in the list.
+
+Input : arr[] = {2, 1, 3}
+Output : 0
+*/
+int Missing(int *arr,int n)
 {
-    int n =sizeof(arr)/sizeof(arr[0]);
-    unorderded_set<int> Ele;
+    //int n =sizeof(arr)/sizeof(arr[0]);
+    unordered_set<int> Ele;
     int Min,Max,res;
     Max=INT_MIN;
     Min=INT_MAX;
@@ -15,5 +27,11 @@ int Missing(int *arr)
         Min=min(Min,arr[i]);
         Ele.insert(arr[i]);
     }
-    return
+    return (Max-Min+1)-(Ele.size());
+}
+int main()
+{
+    int arr[]={7,3,2,6,5};
+    cout<<Missing(arr,5);
+    return 0;
 }
